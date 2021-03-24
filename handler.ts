ee2f25bdfacb6652789c710b2aae838c51104393
Kevin Gao <sudowork@gmail.com>
2021-03-24 01:10:20 -0700

    Log out shots

--- handler.ts
+++ handler.ts
@@ -1,11 +1,19 @@
-module.exports.coffeeSync = async (event) => ({
-    statusCode: 200,
-    body: JSON.stringify(
-        {
-            message: "Go Serverless v1.0! Your function executed successfully!",
-            input: event,
-        },
-        null,
-        2
-    ),
-});
+import { getShots } from "./lib/visualizer";
+
+const SHOTS_PER_BATCH = 5;
+
+module.exports.coffeeSync = async (event: string) => {
+    const shots = await getShots(SHOTS_PER_BATCH);
+    console.log(shots);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v1.0! Your function executed successfully!",
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
+};
