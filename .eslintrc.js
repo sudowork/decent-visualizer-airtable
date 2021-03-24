6ea082202d57996c101688810d545c548e224690
Kevin Gao <sudowork@gmail.com>
2021-03-24 01:08:43 -0700

    Disable no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ["airbnb-typescript/base", "prettier"],
     plugins: ["prettier"],
     rules: {
+        "no-console": "off",
         "prettier/prettier": ["error"],
         "no-use-before-define": "off",
         "@typescript-eslint/no-use-before-define": "off",
